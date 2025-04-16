940ab7ba276ef4f2879b980da81f4a2d2e46e2d3
earthlertkit <earthlertkit@gmail.com>
2025-04-16 14:03:47 -0400

    added thomas algorithm

--- laplace_solver.py
+++ laplace_solver.py
@@ -2,6 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def thomas_algorithm(p, q, r, s):
+
+    n = len(s)
+    rm = np.zeros(n)
+    sm = np.zeros(n)
+    x = np.zeros(n)
+
+    rm[0] = r[0] / q[0]
+    sm[0] = s[0] / q[0]
+
+    # Direct sweep
+    for i in range(1, n):
+        qi = q[i] - p[i] * rm[i - 1]
+        rm[i] = r[i] / qi
+        sm[i] = (s[i] - p[i] * sm[i - 1]) / qi
+    x[-1] = sm[-1]
+
+    # Inverse sweep
+    for i in range(n - 2, -1, -1):
+        x[i] = sm[i] - rm[i] * x[i + 1]
+
+    return x
+
+
 def exact_sol(ix, jx):
 
     # Initializing arrays
@@ -41,6 +65,8 @@ if __name__ == "__main__":
     
     L = 1.0
     W = 1.0
-
+    
+    '''
     # Question 1
     exact_sol(50, 50)
+    ''' 
