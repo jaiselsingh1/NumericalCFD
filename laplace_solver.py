034e282a79afa688bf9cdeb02d755fc2a384a3df
earthlertkit <earthlertkit@gmail.com>
2025-04-16 23:38:22 -0400

    SLOR semi working

--- laplace_solver.py
+++ laplace_solver.py
@@ -229,14 +229,76 @@ def sor(ix, jx):
     plt.legend()
 
 
+def slor(ix, jx):
+
+    # Constants
+    w = 1.8 # relaxation factor
+    res = 1e-5
+    dx = L/(ix-1)
+    dy = W/(jx-1)
+    K = 2*(1/dx**2 + 1/dy**2) # 1/h^2
+    max_iter = 10000
+
+    # Initializing arrays
+    x = np.linspace(0.0, L, ix)
+    y = np.linspace(0.0, W, jx)
+    phi = np.zeros((ix, jx))
+    phi_temp = np.zeros((ix, jx))
+    phi_new = np.zeros((ix, jx))
+    p = np.ones(jx) / dy**2
+    q = np.ones(jx) * -K
+    r = np.ones(jx) / dy**2
+    s = np.zeros(jx)
+    max_res_list = []
+
+    # Boundary Condition
+    phi[:, -1] = np.ones(ix)
+    phi_temp[:, -1] = np.ones(ix)
+    phi_new[:, -1] = np.ones(ix)
+
+    for k in range(max_iter):
+
+        # Sweep in x and solve for each line of constant x using the thomas algorithm (to propagate BCs quickly)
+        for i in range(1, ix-1):
+            s = - (phi[i+1, :] + phi_new[i-1, :]) / dx**2
+            phi_temp[i, :] = thomas_algorithm(p, q, r, s)
+            phi_new[i, 1:-1] = phi[i, 1:-1] + w * (phi_temp[i, 1:-1] - phi[i, 1:-1])
+
+        # Computing max residual
+        max_res = np.max(np.abs(phi_new - phi))
+        max_res_list.append(max_res)
+
+        if max_res < res:
+            break
+
+        # Shifting computation window back
+        phi = phi_new.copy()
+
+    # Plotting
+    plt.figure('SLOR Max Residual')
+    plt.semilogy(np.arange(k+1), max_res_list, label=f"ix = jx = {ix}")
+    plt.xlabel('Iterations')
+    plt.ylabel('Max Residual')
+    plt.legend()
+    
+    plt.figure('SLOR Line Plot')
+    idy = np.argmin(np.abs(y - W/2))
+    plt.plot(x, phi[:, idy], label=f"ix = jx = {ix}")
+    plt.xlabel('x')
+    plt.ylabel('phi(x, W/2)')
+    if ix == 50:
+        plt.plot(x, phi_exact, label="Exact Solution")
+    plt.legend()
+
+
 if __name__ == "__main__":
     
     L = 1.0
     W = 1.0
-    
+
     # Exact Solution
     phi_exact = exact_sol(50, 50)
-    
+    '''
     # Jacobi
     jacobi(50, 50)
     jacobi(100, 100)
@@ -248,5 +310,9 @@ if __name__ == "__main__":
     # SOR
     sor(50, 50)
     sor(100, 100)
+    '''
+    # SLOR
+    slor(50, 50)
+    slor(100, 100)
 
     plt.show()
